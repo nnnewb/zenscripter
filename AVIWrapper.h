f16a323426c8ccaec0e4a88b65fa99ff738dcc45
nnnewb <weak_ptr@outlook.com>
2025-03-10 14:55:04 +0800

    refactor: 格式化代码，添加 CMake 脚本

--- AVIWrapper.h
+++ AVIWrapper.h
@@ -1,5 +1,5 @@
 /* -*- C++ -*-
- * 
+ *
  *  AVIWrapper.h - avifile library wrapper class to play AVI video & audio stream
  *
  *  Copyright (c) 2001-2008 Ogapee. All rights reserved.
@@ -30,45 +30,42 @@
 #include <avm_fourcc.h>
 #include <utils.h>
 
-class AVIWrapper
-{
+class AVIWrapper {
 public:
-    enum { AVI_STOP = 0,
-           AVI_PLAYING = 1
-    };
-    AVIWrapper();
-    ~AVIWrapper();
+  enum { AVI_STOP = 0, AVI_PLAYING = 1 };
+  AVIWrapper();
+  ~AVIWrapper();
 
-    int init( char *filename, bool debug_flag );
-    int initAV( SDL_Surface *surface, bool audio_open_flag );
-    int play( bool click_flag );
+  int init(char *filename, bool debug_flag);
+  int initAV(SDL_Surface *surface, bool audio_open_flag);
+  int play(bool click_flag);
 
-    void audioCallback( void *userdata, Uint8 *stream, int len );
-    int playVideo( void *userdata );
+  void audioCallback(void *userdata, Uint8 *stream, int len);
+  int playVideo(void *userdata);
 
-    unsigned int getWidth(){ return width; };
-    unsigned int getHeight(){ return height; };
+  unsigned int getWidth() { return width; };
+  unsigned int getHeight() { return height; };
 
-private:    
-    double getAudioTime();
-    int drawFrame( avm::CImage *image );
+private:
+  double getAudioTime();
+  int drawFrame(avm::CImage *image);
 
-    SDL_Overlay *screen_overlay;
-    SDL_Rect screen_rect;
-    unsigned int width;
-    unsigned int height;
+  SDL_Overlay *screen_overlay;
+  SDL_Rect screen_rect;
+  unsigned int width;
+  unsigned int height;
 
-    IAviReadFile *i_avi;
-    IAviReadStream *v_stream;
-    IAviReadStream *a_stream;
-    int remaining_count;
-    char *remaining_buffer;
+  IAviReadFile *i_avi;
+  IAviReadStream *v_stream;
+  IAviReadStream *a_stream;
+  int remaining_count;
+  char *remaining_buffer;
 
-    bool debug_flag;
-    int status;
-    SDL_Thread *thread_id;
-    int64_t time_start;
-    double frame_start;
+  bool debug_flag;
+  int status;
+  SDL_Thread *thread_id;
+  int64_t time_start;
+  double frame_start;
 };
 
 #endif // __AVI_WRAPPER_H__
--- Parallel.h
+++ Parallel.h
@@ -25,10 +25,10 @@
 #endif
 
 #ifdef USE_PARALLEL
+#include "SDL_atomic.h"
 #include "SDL_cpuinfo.h"
 #include "SDL_thread.h"
 #include "SDL_timer.h"
-#include "SDL_atomic.h"
 #endif
 
 #ifdef USE_OMP_PARALLEL
@@ -40,122 +40,135 @@
 
 namespace parallel {
 #ifdef USE_OMP_PARALLEL
-  static const int thread_num = omp_get_num_procs();
+static const int thread_num = omp_get_num_procs();
 #elif defined(USE_PARALLEL)
-  static const int thread_num = SDL_GetCPUCount();
+static const int thread_num = SDL_GetCPUCount();
 
-  class ThreadPool {
-    struct Thread {
-      enum Status {
-        IDLE, RUNNING, EXIT
-      };
-      SDL_Thread *thread;
-      struct ThreadData {
-        void(*body)(void *data);
-        void *data;
-        SDL_atomic_t status;
-        SDL_sem *sem;
-      } threadData;
-    };
-    Thread *threads = new Thread[thread_num];
-    int threadNum = 0, threadCreated = 0;
-    inline void createThread() {
-      assert(threadCreated < thread_num);
-      Thread *thread = &threads[threadCreated];
-      thread->threadData.sem = SDL_CreateSemaphore(0);
-      thread->thread = SDL_CreateThread([](void *ptr) {
-        Thread::ThreadData *td = (Thread::ThreadData*) ptr;
-        for (;;) {
-          td->body(td->data);
-          int status = Thread::Status::IDLE;
-          SDL_AtomicSet(&td->status, status);
-          while (status != Thread::Status::RUNNING) {
-            if (status == Thread::Status::EXIT) goto exit_thread;
-            SDL_SemWait(td->sem);
-            status = SDL_AtomicGet(&td->status);
-          }
-        }
-      exit_thread:
-        SDL_DestroySemaphore(td->sem);
-        return 0;}, "Parallel", (void*)&thread->threadData);
-      SDL_DetachThread(thread->thread);
-      ++threadCreated;
-    }
-  public:
-    inline ~ThreadPool() {
-      sync();
-      for (int i = 0; i < threadCreated; ++i) {
-        SDL_AtomicSet(&threads[i].threadData.status, Thread::Status::EXIT);
-        SDL_SemPost(threads[i].threadData.sem);
-      }
-    }
-    inline Thread* newThread(void(*func)(void *body), void* threadData) {
-      assert(threadNum < thread_num);
-      Thread *thread = &threads[threadNum];
-      Thread::ThreadData &td = thread->threadData;
-      td.body = func;
-      td.data = threadData;
-      SDL_AtomicSet(&td.status, Thread::Status::RUNNING);
-      if (threadNum + 1 > threadCreated) createThread();
-      else SDL_SemPost(td.sem);
-      ++threadNum;
-      return thread;
-    }
-    inline void sync() {
-      for (int i = threadNum - 1; i >= 0; --i) {
-        while (SDL_AtomicGet(&threads[i].threadData.status) != Thread::Status::IDLE) SDL_Delay(0);
-        --threadNum;
-      }
-    }
+class ThreadPool {
+  struct Thread {
+    enum Status { IDLE, RUNNING, EXIT };
+    SDL_Thread *thread;
+    struct ThreadData {
+      void (*body)(void *data);
+      void *data;
+      SDL_atomic_t status;
+      SDL_sem *sem;
+    } threadData;
   };
-
-  extern ThreadPool threadPool;
-#endif
-  static int thread_clamp(int threadnum) {
-    if (threadnum > thread_num) threadnum = thread_num;
-    if (threadnum < 1) threadnum = 1;
-    return threadnum;
+  Thread *threads = new Thread[thread_num];
+  int threadNum = 0, threadCreated = 0;
+  inline void createThread() {
+    assert(threadCreated < thread_num);
+    Thread *thread = &threads[threadCreated];
+    thread->threadData.sem = SDL_CreateSemaphore(0);
+    thread->thread = SDL_CreateThread(
+        [](void *ptr) {
+          Thread::ThreadData *td = (Thread::ThreadData *)ptr;
+          for (;;) {
+            td->body(td->data);
+            int status = Thread::Status::IDLE;
+            SDL_AtomicSet(&td->status, status);
+            while (status != Thread::Status::RUNNING) {
+              if (status == Thread::Status::EXIT)
+                goto exit_thread;
+              SDL_SemWait(td->sem);
+              status = SDL_AtomicGet(&td->status);
+            }
+          }
+        exit_thread:
+          SDL_DestroySemaphore(td->sem);
+          return 0;
+        },
+        "Parallel", (void *)&thread->threadData);
+    SDL_DetachThread(thread->thread);
+    ++threadCreated;
   }
 
-  template<typename Body>
-  void For(const int first, const int last, const int step, const Body &body, const int scale = -1) {
-    assert(step > 0);
-    if (last > first) {
-      static const int MINSCALE = 65536 * 4;
-#ifdef USE_OMP_PARALLEL
-      scale > 0 ? omp_set_num_threads(thread_clamp(scale / MINSCALE)) : omp_set_num_threads(thread_num);
-#pragma omp parallel for
-      for (int i = first; i < last; i += step) body(i);
-#elif defined USE_PARALLEL
-      int range = last - first;
-      struct ThreadData {
-        int lr[2];
-        const Body *body;
-      };
-      int nthread = scale > 0 ? thread_clamp(scale / MINSCALE) : thread_num;
-      if (range < nthread) nthread = range;
-      typedef void(*FunctionBody)(void*);
-      FunctionBody* funcs = new FunctionBody[nthread - 1];
-      ThreadData *td = new ThreadData[nthread - 1];
-      int ssize = range / nthread;
-      int lend = last;
-      int i = nthread;
-      while (i > 1) {
-        int lstart = lend - ssize;
-        td[i - 2] = { { lstart, lend }, &body };
-        funcs[i - 2] = [](void* ptr) {
-          ThreadData &td = *((ThreadData*)ptr);
-          for (int i = td.lr[0]; i < td.lr[1]; ++i) (*td.body)(i);};
-        threadPool.newThread(funcs[i - 2], (void*)&td[i - 2]);
-        lend = lstart;
-        --i;
-      }
-      for (int i = first; i < lend; ++i) body(i);
-      threadPool.sync();
-      delete[] td;
-      delete[] funcs;
-#endif
+public:
+  inline ~ThreadPool() {
+    sync();
+    for (int i = 0; i < threadCreated; ++i) {
+      SDL_AtomicSet(&threads[i].threadData.status, Thread::Status::EXIT);
+      SDL_SemPost(threads[i].threadData.sem);
     }
+  }
+  inline Thread *newThread(void (*func)(void *body), void *threadData) {
+    assert(threadNum < thread_num);
+    Thread *thread = &threads[threadNum];
+    Thread::ThreadData &td = thread->threadData;
+    td.body = func;
+    td.data = threadData;
+    SDL_AtomicSet(&td.status, Thread::Status::RUNNING);
+    if (threadNum + 1 > threadCreated)
+      createThread();
+    else
+      SDL_SemPost(td.sem);
+    ++threadNum;
+    return thread;
+  }
+  inline void sync() {
+    for (int i = threadNum - 1; i >= 0; --i) {
+      while (SDL_AtomicGet(&threads[i].threadData.status) != Thread::Status::IDLE)
+        SDL_Delay(0);
+      --threadNum;
+    }
+  }
+};
+
+extern ThreadPool threadPool;
+#endif
+static int thread_clamp(int threadnum) {
+  if (threadnum > thread_num)
+    threadnum = thread_num;
+  if (threadnum < 1)
+    threadnum = 1;
+  return threadnum;
+}
+
+template <typename Body>
+void For(const int first, const int last, const int step, const Body &body, const int scale = -1) {
+  assert(step > 0);
+  if (last > first) {
+    static const int MINSCALE = 65536 * 4;
+#ifdef USE_OMP_PARALLEL
+    scale > 0 ? omp_set_num_threads(thread_clamp(scale / MINSCALE)) : omp_set_num_threads(thread_num);
+#pragma omp parallel for
+    for (int i = first; i < last; i += step)
+      body(i);
+#elif defined USE_PARALLEL
+    int range = last - first;
+    struct ThreadData {
+      int lr[2];
+      const Body *body;
+    };
+    int nthread = scale > 0 ? thread_clamp(scale / MINSCALE) : thread_num;
+    if (range < nthread)
+      nthread = range;
+    typedef void (*FunctionBody)(void *);
+    FunctionBody *funcs = new FunctionBody[nthread - 1];
+    ThreadData *td = new ThreadData[nthread - 1];
+    int ssize = range / nthread;
+    int lend = last;
+    int i = nthread;
+    while (i > 1) {
+      int lstart = lend - ssize;
+      td[i - 2] = {{lstart, lend}, &body};
+      funcs[i - 2] = [](void *ptr) {
+        ThreadData &td = *((ThreadData *)ptr);
+        for (int i = td.lr[0]; i < td.lr[1]; ++i)
+          (*td.body)(i);
+      };
+      threadPool.newThread(funcs[i - 2], (void *)&td[i - 2]);
+      lend = lstart;
+      --i;
+    }
+    for (int i = first; i < lend; ++i)
+      body(i);
+    threadPool.sync();
+    delete[] td;
+    delete[] funcs;
+#endif
   }
 }
+} // namespace parallel
 #endif
